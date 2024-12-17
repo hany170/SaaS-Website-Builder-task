ba9ad21c51f46b6abf10908049e68f0373b40f37
faridaelsayaad <119070107+faridaelsayaad@users.noreply.github.com>
2024-12-17 21:57:21 +0200

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,15 @@
+// next.config.js (ES Module syntax)
+const nextConfig = {
+  images: {
+    domains: [
+      'uploadthing.com',
+      'utfs.io',
+      'img.clerk.com',
+      'subdomain',
+      'files.stripe.com',
+    ],
+  },
+  reactStrictMode: false,
+};
+
+export default nextConfig;
